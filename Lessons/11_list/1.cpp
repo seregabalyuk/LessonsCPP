ee7c64fc0a73dd2948f5d8c35d54e3b47f7683d3
Serega Balyuk <seregabalyuk@gmail.com>
2026-04-27 15:50:37 +0800

    add list homework and lesson

--- Lessons/11_list/1.cpp
+++ Lessons/11_list/1.cpp
@@ -1,7 +1,93 @@
 #include <list>
+#include <iostream>
 
-using namespace std;
+struct MyStruct{
+  MyStruct(int, int, int) {};
+};
 
 int main() {
+  // std::list<int> mylist = {1, 2, 3, 4};
+  // std::list<int> a(10, 1); // 10 elements = 1
+  // std::list<int> b(10); // 10 elements
+  // std::list<int> c;
+  // std::list<MyStruct> my;
   
+  // size_t s = c.size(); // -> size
+  // bool f = c.empty(); // true if empty
+  
+  // c.emplace_back(10);  // add backs
+  // c.emplace_front(11); // add front
+
+  // my.emplace_back(0, 1, 2);
+
+  // my.pop_back(); // delete back
+  // c.pop_front(); // delete front;
+  
+  // c.front() = 10;
+  // int g = c.back();
+
+  // c.clear(); // clean all elements
+
+  // std::list<int>::iterator it1 = c.begin(); // first element
+  // auto it2 = c.end(); // after back element
+
+  // int massive[100];
+
+  // for (size_t i = 0; i != 100; ++ i) {
+  //   ++ i; // i += 1; return i;
+  //   i ++; // int j = i; i += 1; return j;
+  // }
+
+  
+
+  // auto iterMax = mylist.begin();
+  // for (auto iter = mylist.begin(); iter != mylist.end(); ++ iter) {
+  //   if (*iter > *iterMax) {
+  //     iterMax = iter;
+  //   }
+  // }
+
+  // iterMax --; //
+  // {
+  //   const std::list<float> constants = {3.14, 2.7, 1};
+
+  //   std::list<float>::const_iterator it = constants.begin();
+
+  //   for (auto iter = mylist.cbegin(); iter != mylist.cend(); ++ iter) {
+  //     if (*iter > *iterMax) {
+  //     //  iter = iterMax;
+  //     }
+  //   }
+  // }
+
+  {
+    std::list<int> mylist = {1, 2, 3, 4} ;
+    auto iterMax = -- mylist.end();
+    mylist.emplace(iterMax, 10); // mylist = {1, 2, 3, 10, 4};
+
+    mylist.emplace(mylist.begin(), 9); // mylist = {9, 1, 2, 3, 10, 4};
+    mylist.emplace(mylist.end(), 8); // mylist = {9, 1, 2, 3, 10, 4, 8};
+
+    std::list<int> mylist2 = {0, -1, -2} ;
+    mylist.splice(mylist.begin(), mylist2); 
+    // mylist = {0, -1, -2, 9, 1, 2, 3, 10, 4, 8};
+    // mylist2 = {};
+    for (auto value: mylist) {
+      std::cout << value << ' ';
+    }
+    std::cout << "\n";
+    
+
+    mylist.sort();
+    // mylist[10] is deleted;
+
+    
+
+    mylist.erase(iterMax); // 
+  }
+
+ 
+  
+  
+ // std::cout << *iterMax;
 }
--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 5) - [x] [Задание на понимание устройства множественного наследования](HomeWorks/5_multy_inheritance/multy_inheritance.md) (если не получается, ничего страшного, я на уроке объясню)
 6) - [x] [Задание на union](HomeWorks/6_union/union.md)
 7) - [ ] [Создать свой физический движок (продолжение)](HomeWorks/7_PhysicsEngine/PhysicsEngine.md)
+8) - [ ] [Задачи на list](HomeWorks/8_list/list.md)
